6d28ca8662e36f228b50a5da1c76aad30938bc4c
nicolazin <106328512+nicolazin@users.noreply.github.com>
2023-11-17 10:36:40 -0300

    final

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1];
     const idAudio = '#som_${efeito}';
-    tecla.oneclick = function() {
+    tecla.onclick = function() {
         tocaSom(idAudio);
     };
 }
