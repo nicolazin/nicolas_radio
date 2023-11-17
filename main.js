1e5c047ae3ee1683c29bc4562ef09a7b86e20c4d
nicolazin <106328512+nicolazin@users.noreply.github.com>
2023-11-17 10:23:45 -0300

    final

---
+++ main.js
@@ -0,0 +1,12 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = function(){
+        tocaSom(idAudio);
+    }
+}
